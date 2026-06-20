b4fb5d9a1d0286447c342914be57c45ad8bd11dd
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-06-20 22:56:12 +0530

    Solved first coding question

---
+++ JobSwitchsln/ConsoleApp1/Topics/CodingQuePractise/CodingQuesC#.cs
@@ -0,0 +1,39 @@
+﻿using PrepInterview;
+
+namespace ReviseConcepts.Topics.CodingQuePractise
+{
+    [Topic("Coding Questions", "C#")]
+    public class CodingQues: IRunnable
+    {
+        public void Run() 
+        {
+            ReverseString();
+        }
+
+
+        //Reverse the given string
+
+        void ReverseString()
+        {
+            string s = "hello";
+            //Direct 
+            //Console.WriteLine(s.Reverse().ToArray());
+
+            //Indirect
+            char[] arr = s.ToCharArray();
+
+            int l = 0;
+            int r = arr.Length - 1;
+
+            while (l < r) 
+            {
+                (arr[l], arr[r]) = (arr[r], arr[l]);
+                l ++;
+                r --;
+            }
+
+            Console.WriteLine(new string(arr));
+        
+        }
+    }
+}
