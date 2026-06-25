18331aadb5eb6d99f328130488b5eeae53af1475
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-06-25 12:52:53 +0530

    5 coding questions for 25th June

--- JobSwitchsln/ConsoleApp1/Topics/CodingQuePractise/CodingQuesC#.cs
+++ JobSwitchsln/ConsoleApp1/Topics/CodingQuePractise/CodingQuesC#.cs
@@ -14,6 +14,17 @@ namespace ReviseConcepts.Topics.CodingQuePractise
             CheckStringPalindrome("aab1baa");
 
             CountVowelsAndConsonants("Vaishnavi");
+
+            bool isAnagram = CheckAnagram("BaD", "DaD");
+            Console.WriteLine($"Given strings are anagrams?: {isAnagram}");
+
+            CountOccurance("Vaishnavi", 'a');
+
+            RemoveStrings("Vaishnavi has abundance of intelligence");
+
+            RevWordsInSentence("Vaishnavi is Great");
+
+            FirstSingleChar("aabbcde");
         }
 
 
@@ -121,5 +132,86 @@ namespace ReviseConcepts.Topics.CodingQuePractise
 
         }
 
+
+
+        // Check if strings are anagram 
+        // make lower and compare using sequence equal
+        bool CheckAnagram(string a, string b)
+        {
+            Console.WriteLine("\n\n\n");
+
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            return a.ToLower().OrderBy(c => c).SequenceEqual(b.ToLower().OrderBy(c => c));
+        }
+
+
+
+        // Count occurance of character
+        // use Count func with lambda ex
+        void CountOccurance(string str, char c)
+        {
+            Console.WriteLine("\n\n\n");
+
+            int count = str.Count(x => x == c);
+
+            Console.WriteLine($"Count of {c} in {str} is {count}");
+        }
+
+
+
+        // Remove all spaces from string
+        // use linq 
+        void RemoveStrings(string str)
+        {
+            Console.WriteLine("\n\n\n");
+
+            //var a = string.Concat(str.Split(' '));
+
+            var a = new string(str.Where(x => x != ' ').ToArray());
+
+            Console.WriteLine($"Concatinated string is: {a}");
+        }
+
+
+
+        // Reverse words in a sentence 
+        // direct Reverse or while to reverse
+        void RevWordsInSentence(string str)
+        {
+            Console.WriteLine("\n\n\n");
+
+            var w = str.Split(' ');
+
+            //var ans = String.Join(" ", w.Reverse()); // Direct implementation
+
+            int l = 0, r = w.Length - 1;
+
+            while (l < r)
+            {
+                (w[l], w[r]) = (w[r], w[l]);
+                l += 1;
+                r -= 1;
+            }
+
+            var ans = String.Join(" ", w);
+
+            Console.WriteLine(ans);
+        }
+
+
+
+        // First non-repeating character
+        void FirstSingleChar(string s)
+        {
+            Console.WriteLine("\n\n\n");
+            var ans = s.GroupBy(c => c)
+            .FirstOrDefault(g => g.Count() == 1)?.Key ?? '\0';
+
+            Console.WriteLine($"First non-repeating character is: {ans}");
+        }
     }
 }
