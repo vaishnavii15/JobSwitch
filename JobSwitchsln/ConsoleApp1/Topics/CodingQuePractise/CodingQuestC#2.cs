cd1d52e5c8597a05fb772eac800d697e175ebf45
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-06-27 14:25:07 +0530

    27th coding questions

--- JobSwitchsln/ConsoleApp1/Topics/CodingQuePractise/CodingQuestC#2.cs
+++ JobSwitchsln/ConsoleApp1/Topics/CodingQuePractise/CodingQuestC#2.cs
@@ -13,6 +13,16 @@ namespace ReviseConcepts.Topics.CodingQuePractise
 
             var rotationString = StringRotationOfOthers("abcdef", "defabcz");
             Console.WriteLine($"Is string rotation of others: {rotationString}");
+
+            RemoveDuplChars("abccdeeav");
+
+            LongestWord("Hello my nameee is Vaishnavi!");
+
+            FreqofChar("abccddaabz");
+
+            ReplaceOccurance("good", "boy");
+
+            CheckIfSubstring("abcccd", "cc");
         }
 
         // Count words in string 
@@ -64,6 +74,88 @@ namespace ReviseConcepts.Topics.CodingQuePractise
             }
 
             return false;
+        }
+
+
+
+        // Remove duplicate characters from string
+        // just get the distinct, make arr, convert to string
+        void RemoveDuplChars(string s)
+        {
+            Console.WriteLine("\n\n\n");
+
+            var ans = new string(s.Distinct().ToArray());
+
+            Console.WriteLine(ans);
+        }
+
+
+
+        // Find longest word in sentence
+        // split, order by descending
+        void LongestWord(string s)
+        {
+            Console.WriteLine("\n\n\n");
+
+            var charArr = s.Split(" ");
+
+            var ans = charArr.OrderByDescending(c=> c.Length).First();
+
+            Console.WriteLine($"Longest word in sentence: {ans}");
+        }
+
+
+
+        // Count frequency of each character 
+        // groupby and count
+        void FreqofChar(string s)
+        {
+            Console.WriteLine("\n\n\n");
+
+            //s.GroupBy(c => c).ToList().ForEach(c => Console.WriteLine($"{c.Key} -> {c.Count()}"));
+
+            var ans = s.GroupBy(c => c);
+
+            foreach (var i in ans)
+            {
+                Console.WriteLine($"{i.Key} -> {i.Count()}");
+            }
+        }
+
+
+
+        // Replace all occurance of word
+        // split and replace
+        void ReplaceOccurance(string a, string b)
+        {
+            Console.WriteLine("\n\n\n");
+
+            string s = "Buddy (the dog) is a good boy";
+
+            var v= s.Split(" ");
+
+            for (int i = 0; i<= v.Length-1; i++)
+            {
+                if (v[i] == a)
+                {
+                    v[i] = b;
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", v));
+        }
+
+
+
+        // Check if string is subsctring of others
+        // use contains
+        void CheckIfSubstring(string a, string b)
+        {
+            Console.WriteLine("\n\n\n");
+
+            var ans = a.Contains(b);
+
+            Console.WriteLine($"Is b substring of a: {ans}");
         }
     }
 }
