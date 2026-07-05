1ad2980591f81be05f211e0744b2d6f9bcecd681
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-07-05 14:34:59 +0530

    Generic class

--- JobSwitchsln/ConsoleApp1/Topics/Memory/Generics.cs
+++ JobSwitchsln/ConsoleApp1/Topics/Memory/Generics.cs
@@ -13,7 +13,17 @@ namespace ReviseConcepts.Topics.Memory
             string firstString = GetFirst(new string[] { "A", "B" });
 
             Console.WriteLine($"Int type: {firstInt}, String type: {firstString}");
+
+            // 2
+            Box<int> intBox = new Box<int>();
+            intBox.Set(42);
+
+            Box<string> stringBox = new Box<string>();
+            stringBox.Set("Hii!!");
         }
+
+
+        //Write one piece of code that works for any data type — without sacrificing type safety.
 
 
         //1 - One method any type
@@ -22,5 +32,15 @@ namespace ReviseConcepts.Topics.Memory
         {
             return array[0];
         }
+    }
+
+
+    // 2 - Generic class
+    public class Box<T>
+    {
+        private T _value;
+
+        public void Set(T value) => _value = value;
+        public T Get() => _value;
     }
 }
