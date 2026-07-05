d3bab4741522dd1ed797d78343b6240e8a33b85b
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-07-05 14:01:14 +0530

    Generics

---
+++ JobSwitchsln/ConsoleApp1/Topics/Memory/Generics.cs
@@ -0,0 +1,26 @@
+﻿using PrepInterview;
+
+namespace ReviseConcepts.Topics.Memory
+{
+    [Topic("Memory", "Generics")]
+
+    public class Generics: IRunnable
+    {
+        public void Run()
+        {
+            // 1
+            int firstInt = GetFirst(new int[] { 1, 2, 3 });
+            string firstString = GetFirst(new string[] { "A", "B" });
+
+            Console.WriteLine($"Int type: {firstInt}, String type: {firstString}");
+        }
+
+
+        //1 - One method any type
+        // Basic generics implementation
+        public T GetFirst<T>(T[] array)
+        {
+            return array[0];
+        }
+    }
+}
