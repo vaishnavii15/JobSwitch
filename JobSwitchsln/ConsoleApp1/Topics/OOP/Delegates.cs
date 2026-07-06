776e2d54e758f86b57d818761134cca4333f1853
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-07-06 19:23:19 +0530

    Delegates implementation

---
+++ JobSwitchsln/ConsoleApp1/Topics/OOP/Delegates.cs
@@ -0,0 +1,33 @@
+﻿using PrepInterview;
+
+namespace ReviseConcepts.Topics.OOP
+{
+    [Topic("OOPs", "Delegates")]
+
+    public class Delegates: IRunnable
+    {
+        public void Run()
+        {
+            MathOperation op = Add; // Assigned delegate to Add method
+
+            int result = op(3, 4);
+
+            Console.WriteLine($"Add delegate result: {result}");
+
+            Console.WriteLine("\n\n\n");
+
+            op = Multiply;         // Assigned delegate to multiply method
+
+            result = op(3, 4);
+
+            Console.WriteLine($"Multiply delegate result: {result}");
+        }
+
+        public delegate int MathOperation(int a, int b);
+
+        public int Add(int a, int b) => a + b;
+
+        public int Multiply(int a, int b) => a * b;
+
+    }
+}
