340645ed444f1f03b0767818753224de89db1f10
Vaishnavii Dayal <vaishnaviidayal15@gmail.com>
2026-07-09 15:23:47 +0530

    Fixed schema

---
+++ JobSwitchsln/MVCApplication/Models/InterviewEntryViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApplication.Models;
+
+public class InterviewEntryViewModel
+{
+    [Required]
+    [Display(Name = "Company Name")]
+    public string CompanyName { get; set; } = string.Empty;
+
+    public string? Website { get; set; }
+
+    public string? Industry { get; set; }
+
+    [Required]
+    [Display(Name = "Role Title")]
+    public string RoleTitle { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "Applied Date")]
+    public DateTime AppliedDate { get; set; } = DateTime.Today;
+
+    [Display(Name = "Job Link")]
+    public string? JobLink { get; set; }
+
+    [Display(Name = "Salary Expectation")]
+    public decimal? SalaryExpectation { get; set; }
+
+    [Required]
+    public string Status { get; set; } = "Applied";
+
+    public string? Notes { get; set; }
+
+    public List<JobApplication> Applications { get; set; } = new();
+}
--- JobSwitchsln/MVCApplication/Models/InterviewRound.cs
+++ JobSwitchsln/MVCApplication/Models/InterviewRound.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace MVCApplication.Models;
+﻿namespace MVCApplication.Models;
 
 public partial class InterviewRound
 {
